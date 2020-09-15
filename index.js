045b1ddd3349ebab85ee3543080e9312ab3c874d
Wazeed@Wazeed-PC <wajeed.cse2014@gmail.com>
2020-09-15 23:54:52 +0530

    Posting data to an external api using fetch API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 document.getElementById("gettext").addEventListener("click", getText);
 document.getElementById("getusers").addEventListener("click", getUsers);
 document.getElementById("getPosts").addEventListener("click", getPosts);
+document.getElementById("addPost").addEventListener("submit", addPost);
 
 function getText(){
     // fetch("sample.txt")
@@ -22,13 +23,13 @@ function getUsers(){
     fetch("users.json")
     .then((res) => res.json())
     .then((data) => {
-        let output = '<h2>Users</h2>';
+        let output = '<h2 class="mb-4">Users</h2>';
         data.forEach(user => {
             output += `
-                <ul>
-                    <li>ID: ${user.id}</li>
-                    <li>Name: ${user.name}</li>
-                    <li>Email: ${user.email}</li>
+                <ul class="list-group mb-3">
+                    <li class="list-group-item">ID: ${user.id}</li>
+                    <li class="list-group-item">Name: ${user.name}</li>
+                    <li class="list-group-item">Email: ${user.email}</li>
                 </ul>
             `;
         });
@@ -39,10 +40,10 @@ function getPosts(){
     fetch("https://jsonplaceholder.typicode.com/posts")
     .then((res) => res.json())
     .then((data) => {
-        let output = '<h2>Posts</h2>';
+        let output = '<h2 class="mb-4">Posts</h2>';
         data.forEach(post => {
             output += `
-                <div>
+                <div class="card card-body mb-3">
                     <h3>${post.title}</h3>
                     <p>${post.body}</p>
                 </div>
@@ -50,4 +51,20 @@ function getPosts(){
         });
         document.getElementById("output").innerHTML = output;
     })
+}
+function addPost(e){
+    e.preventDefault();
+    let title = document.getElementById("title").value;
+    let body = document.getElementById("body").value;
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: 'POST',
+        headers: {
+            'Accept':'application/json, text/plain, */*',
+            'Content-type':'application/json'
+        },
+        body:JSON.stringify({title:title, body:body})
+    })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 }
