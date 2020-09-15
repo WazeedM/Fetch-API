c2f98276f38bfd6f9ada078b1edc1f9c83ba872a
Wazeed@Wazeed-PC <wajeed.cse2014@gmail.com>
2020-09-15 21:00:28 +0530

    fetching user data from json file using fetch API

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-document.getElementById("getitem").addEventListener("click", getItems);
+document.getElementById("gettext").addEventListener("click", getText);
+document.getElementById("getusers").addEventListener("click", getUsers);
 
-function getItems(){
+function getText(){
+    // fetch("sample.txt")
+    // .then(function(res){
+    //     return res.text();
+    //  })
+    //  .then(function(data){
+    //      console.log(data);
+    //  })
     fetch("sample.txt")
-    .then(function(res){
-        return res.text();
-     })
-     .then(function(data){
-         console.log(data);
-     })
+    .then((res) => res.text())
+    .then((data) => {
+        document.getElementById("output").innerHTML = data;
+    })
+    .catch((err) => console.log(err))
+}
+
+function getUsers(){
+    fetch("users.json")
+    .then((res) => res.json())
+    .then((data) => {
+        let output = '<h2>Users</h2>';
+        data.forEach(user => {
+            output += `
+                <ul>
+                    <li>ID: ${user.id}</li>
+                    <li>Name: ${user.name}</li>
+                    <li>Email: ${user.email}</li>
+                </ul>
+            `;
+        });
+        document.getElementById("output").innerHTML = output;
+    })
 }
